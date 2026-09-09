e6937a4a33b375ecc14f4ada9237a9fb6c5cc34c
yabajackson <yazxjack56@gmail.com>
2026-09-09 11:53:32 +0300

    Complete triangle area assignment

--- Chapter_02/triangle_area.cpp
+++ Chapter_02/triangle_area.cpp
@@ -4,3 +4,38 @@
 // Area = sqrt(s(s-side1)(s-side2)(s-side3))
 // Example: Enter three points for a triangle: 1.5, -3.4, 4.6, 5, 9.5, -3.4 
 // The area of the triangle is 33.6
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double x1, y1;
+    double x2, y2;
+    double x3, y3;
+
+    cout << "Enter three points for a triangle "
+         << "(x1 y1 x2 y2 x3 y3): ";
+
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+    // Calculate the length of each side using the distance formula.
+    double side1 = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    double side2 = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
+    double side3 = sqrt(pow(x1 - x3, 2) + pow(y1 - y3, 2));
+
+    // Calculate the semi-perimeter.
+    double s = (side1 + side2 + side3) / 2.0;
+
+    // Calculate the area using Heron's formula.
+    double area = sqrt(
+        s * (s - side1) * (s - side2) * (s - side3)
+    );
+
+    cout << fixed << setprecision(1);
+    cout << "The area of the triangle is " << area << endl;
+
+    return 0;
+}
