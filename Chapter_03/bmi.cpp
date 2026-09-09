2163ab6e90e5714f8445ef5d5bd4eacb69a78a17
yabajackson <yazxjack56@gmail.com>
2026-09-09 12:15:44 +0300

    Complete BMI calculator assignment

--- Chapter_03/bmi.cpp
+++ Chapter_03/bmi.cpp
@@ -13,3 +13,60 @@
 // Enter height in inches: 70
 // BMI is 20.95
 // Normal
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double weightInPounds;
+    double heightInInches;
+
+    const double POUNDS_TO_KILOGRAMS = 0.45359237;
+    const double INCHES_TO_METERS = 0.0254;
+
+    cout << "Enter weight in pounds: ";
+    cin >> weightInPounds;
+
+    cout << "Enter height in inches: ";
+    cin >> heightInInches;
+
+    if (weightInPounds <= 0 || heightInInches <= 0)
+    {
+        cout << "Weight and height must be greater than zero." << endl;
+        return 1;
+    }
+
+    double weightInKilograms =
+        weightInPounds * POUNDS_TO_KILOGRAMS;
+
+    double heightInMeters =
+        heightInInches * INCHES_TO_METERS;
+
+    double bmi =
+        weightInKilograms / pow(heightInMeters, 2);
+
+    cout << fixed << setprecision(1);
+    cout << "The BMI is " << bmi << endl;
+
+    if (bmi < 18.5)
+    {
+        cout << "Underweight" << endl;
+    }
+    else if (bmi < 25.0)
+    {
+        cout << "Normal" << endl;
+    }
+    else if (bmi < 30.0)
+    {
+        cout << "Overweight" << endl;
+    }
+    else
+    {
+        cout << "Obese" << endl;
+    }
+
+    return 0;
+}
